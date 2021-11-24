1e9d098b119e760340dd5487b4046bf19224abd0
jorge moreno <morenojorge308@outlook.com>
2021-11-24 09:58:32 -0600

    InicioProyecto

---
+++ ProyectoPostItProgramacionWeb/Controllers/NotasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPostItProgramacionWeb.Controllers
+{
+    public class NotasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
