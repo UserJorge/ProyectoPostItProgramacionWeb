ddfe04632b87fbb3faa6938b9083b06d2bfcec76
jorge moreno <morenojorge308@outlook.com>
2021-12-03 21:09:02 -0600

    UserPages

---
+++ ProyectoPostItProgramacionWeb/Areas/Usuario/Controllers/PostItController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPostItProgramacionWeb.Areas.Usuario.Controllers
+{
+    public class PostItController : Controller
+    {
+        [Route("Usuario/Index")]
+        [Route("Usuario/")]
+        [Route("Usuario/Home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Usuario/Agregar/")]
+        public IActionResult AgregarPostIt()
+        {
+            return View();
+        }
+        [Route("Usuario/Editar/{id}")]
+        public IActionResult EditarPostIt(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EditarPostIt()
+        {
+            return View();
+        }
+        [Route("Usuario/Eliminar/{id}")]
+        public IActionResult EliminarPostIt(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EliminarPostIt()
+        {
+            return View();
+        }
+    }
+}
--- ProyectoPostItProgramacionWeb/Startup.cs
+++ ProyectoPostItProgramacionWeb/Startup.cs
@@ -16,6 +16,7 @@ namespace ProyectoPostItProgramacionWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc();
         }
 
@@ -26,18 +27,13 @@ namespace ProyectoPostItProgramacionWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseRouting();
             app.UseFileServer();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
-                endpoints.MapControllerRoute(name: "default", pattern: "{controller=Notas}/{action=Index}/{id?}");
-             
-             
-            });
-          
+                endpoints.MapControllerRoute(name: "areas", pattern: "{areas:exists}/{controller=Notas}/{action=Index}/{id?}");                       
+            });      
         }
     }
 }
