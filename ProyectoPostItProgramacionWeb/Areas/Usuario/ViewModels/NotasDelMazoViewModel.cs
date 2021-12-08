89ebc2b7f6d1b7da356411f3ce6b2acea8f4b13f
jorge moreno <morenojorge308@outlook.com>
2021-12-07 19:53:42 -0600

    CambiosSelect

---
+++ ProyectoPostItProgramacionWeb/Areas/Usuario/ViewModels/NotasDelMazoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoPostItProgramacionWeb.Areas.Usuario.ViewModels
+{
+    public class NotasDelMazoViewModel
+    {
+        public IEnumerable<Models.Nota> Notas { get; set; }
+        public IEnumerable<Models.Mazo> Mazos { get; set; }
+        public int Mazo { get; set; }
+    }
+}
